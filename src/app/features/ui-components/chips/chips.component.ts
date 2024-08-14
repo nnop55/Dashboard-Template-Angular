b8aa3f8636577f11fab5e3734bb8a78b40d653f6
nnop55 <tornikevasiko@gmail.com>
2024-08-14 16:32:33 +0400

    any

--- src/app/features/ui-components/chips/chips.component.ts
+++ src/app/features/ui-components/chips/chips.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BreadcrumbComponent } from '../../../shared/components/breadcrumb/breadcrumb.component';
+import { ChipItemComponent } from '../../../shared/components/chip-item/chip-item.component';
 
 @Component({
   selector: 'app-chips',
   standalone: true,
-  imports: [],
+  imports: [BreadcrumbComponent, ChipItemComponent],
   templateUrl: './chips.component.html',
   styleUrl: './chips.component.scss'
 })
--- src/app/shared/components/chip-item/chip-item.component.ts
+++ src/app/shared/components/chip-item/chip-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, signal } from '@angular/core';
+import { Component, computed, Input, OnInit, signal } from '@angular/core';
 import { MatAutocompleteModule, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -8,39 +8,69 @@ import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-chips',
+  selector: 'app-chip-item',
   standalone: true,
   imports: [MatChipsModule, MatAutocompleteModule, MatFormFieldModule, MatIconModule, ReactiveFormsModule],
-  templateUrl: './chips.component.html',
-  styleUrl: './chips.component.scss'
+  templateUrl: './chip-item.component.html',
+  styleUrl: './chip-item.component.scss'
 })
-export class ChipsComponent {
+export class ChipItemComponent implements OnInit {
   @Input({ required: true }) type!: ChipsTypes;
-  @Input() data = signal<any[]>([]);
+  @Input() data: any[] = [
+    {
+      isDisabled: false,
+      text: 'Apple'
+    },
+    {
+      isDisabled: false,
+      text: 'Lemon'
+    },
+    {
+      isDisabled: false,
+      text: 'Lime'
+    },
+    {
+      isDisabled: false,
+      text: 'Orange'
+    },
+    {
+      isDisabled: false,
+      text: 'Strawberry'
+    }
+  ];
+  @Input() label!: string;
 
-  readonly allItems: string[] = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
+  selectedItems = signal<any[]>([]);
+
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
   readonly filteredData = computed(() => {
     const current = this.itemCtrl.value?.toLowerCase();
     return current
-      ? this.allItems.filter(item => item.toLowerCase().includes(current))
-      : this.allItems.slice();
+      ? this.data.filter(item => item.text.toLowerCase().includes(current))
+      : this.data.slice();
   });
 
   itemCtrl = new FormControl('');
+
+  ngOnInit(): void {
+    if (this.data.length && this.data.length > 0) {
+      this.selectedItems.set([this.data[0].text])
+    }
+  }
 
   add(event: MatChipInputEvent): void {
     const value = (event.value || '').trim();
 
     if (value) {
-      this.data.update(items => [...items, value]);
+      this.selectedItems.update(items => [...items, value]);
     }
 
     this.itemCtrl.setValue('');
+    console.log(this.itemCtrl.value)
   }
 
   remove(item: string): void {
-    this.data.update(items => {
+    this.selectedItems.update(items => {
       const index = items.indexOf(item);
       if (index < 0) {
         return items;
@@ -52,7 +82,7 @@ export class ChipsComponent {
   }
 
   selected(event: MatAutocompleteSelectedEvent): void {
-    this.data.update(items => [...items, event.option.viewValue]);
+    this.selectedItems.update(items => [...items, event.option.viewValue]);
     this.itemCtrl.setValue('');
     event.option.deselect();
   }
